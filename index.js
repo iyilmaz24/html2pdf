dfa9feb8a0cacbb6fa281c8ad1f1e41fa0282d84
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-27 14:05:21 -0500

    add final code for printing a html div

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 
-window.addEventListener("DOMContentLoaded", () => {
 
-    function printFunc() {
-        console.log("Button Clicked");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+
+    function printDiv(tagid) {
+    
+        // hashid is our "selector" for jQuery searches
+        let hashid = "#" + tagid;
+        let tagname =  $(hashid).prop("tagName").toLowerCase() ;
+        let attributes = ""; 
+        let attrs = document.getElementById(tagid).attributes;
+          $.each(attrs,function(i,elem){
+            attributes +=  " "+  elem.name+" ='"+elem.value+"' " ;
+          })
+        let divToPrint= $(hashid).html() ;
+        let head = "<html><head>"+ $("head").html() + "</head>" ;
+        let allcontent = head + "<body  onload='window.print()' >"+ "<" + tagname + attributes + ">" +  divToPrint + "</" + tagname + ">" +  "</body></html>"  ;
+        let newWin = window.open('','Print-Window');
+        newWin.document.open();
+        newWin.document.write(allcontent);
+        newWin.document.close();
     }
 
-    $("#printBtn").click(() => printFunc())
+    jqTest = $("#printBtn")
+    console.log(jqTest)
+    $("#printBtn").on("click", () => {printDiv("printDiv")});
 
+    
 });
+
